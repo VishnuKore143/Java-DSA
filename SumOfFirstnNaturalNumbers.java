eb2ac0e20030f41235a1217b8e62f297f9486c35
VishnKore143 <vishnukore123@gmail.com>
2022-10-12 03:59:26 +0530

    Algo

---
+++ SumOfFirstnNaturalNumbers.java
@@ -0,0 +1,11 @@
+package AnalysisOfAlgorithms;
+
+public class SumOfFirstnNaturalNumbers {
+    public static void main(String[] args) {
+
+        System.out.println(fun1(3));
+    }
+    static int fun1(int n){
+        return n*(n+1)/2;
+    }
+}
